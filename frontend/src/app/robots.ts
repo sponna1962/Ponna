a7b98f99f692ac477c27440391a416cfa549fbe1
sponna1962 <sponna1962@gmail.com>
2026-09-17 08:25:14 +0530

    Add robots.txt configuration

---
+++ frontend/src/app/robots.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [
+        '/admin/',
+        '/dashboard/',
+        '/profile/',
+        '/devices/',
+        '/diagnostic/',
+        '/login/',
+        '/shared/',
+      ],
+    },
+    sitemap: 'https://www.ponna.in/sitemap.xml',
+  }
+}
